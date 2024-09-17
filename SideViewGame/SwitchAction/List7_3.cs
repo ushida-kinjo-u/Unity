21b77ad749a83076220abc370d772a17a55ec156
ushida-kinjo-u <109647066+ushida-kinjo-u@users.noreply.github.com>
2024-09-17 15:40:07 +0900

    Create List7_3.cs

---
+++ SideViewGame/SwitchAction/List7_3.cs
@@ -0,0 +1,54 @@
+// List 7-3 スイッチのゲームオブジェクトを作ろう
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchAction : MonoBehaviour
+{
+    public GameObject targetMoveBlock;
+    public Sprite imageOn;
+    public Sprite imageOff;
+    public bool on = false; // スイッチの状態(true:押されている false:押されていない)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (on)
+        {
+            GetComponent<SpriteRenderer>().sprite = imageOn;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = imageOff;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // 接触開始
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (on)
+            {
+                on = false;
+                GetComponent<SpriteRenderer>().sprite = imageOff;
+                MovingBlock movBlock = targetMoveBlock.GetComponent<MovingBlock>();
+                movBlock.Stop();
+            }
+            else
+            {
+                on = true;
+                GetComponent<SpriteRenderer>().sprite = imageOn;
+                MovingBlock movBlock = targetMoveBlock.GetComponent<MovingBlock>();
+                movBlock.Move();
+            }
+        }
+    }
+}
