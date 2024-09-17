d46aea12c75ac69a35609ddbefe6d0f152366cbc
ushida-kinjo-u <109647066+ushida-kinjo-u@users.noreply.github.com>
2024-09-17 15:38:56 +0900

    Create List6_9.cs

---
+++ SideViewGame/ResultManager/List6_9.cs
@@ -0,0 +1,23 @@
+// List 6-9 トータルスコアを表示するスクリプトを作ろう
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour
+{
+    public GameObject scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = GameManager.totalScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
