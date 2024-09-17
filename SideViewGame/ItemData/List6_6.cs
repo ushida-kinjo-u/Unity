934b5d756dee651f55ee826b5062e043899d526b
ushida-kinjo-u <109647066+ushida-kinjo-u@users.noreply.github.com>
2024-09-17 14:58:00 +0900

    Create List6_6.cs

---
+++ SideViewGame/ItemData/List6_6.cs
@@ -0,0 +1,22 @@
+// List 6-6 アイテムデータスクリプト（ItemData）を作ろう
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : MonoBehaviour
+{
+    public int value = 0;       // 整数値を設定できる
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
