4dfa28a1c212dc064630a9f767f8dd3c44f4495f
ushida-kinjo-u <109647066+ushida-kinjo-u@users.noreply.github.com>
2025-10-21 14:10:45 +0900

    Update List3_2.cs

--- SideViewGame/PlayerController/List3_2.cs
+++ SideViewGame/PlayerController/List3_2.cs
@@ -1,45 +1,42 @@
-// List 3-2 Unityエディターからパラメーターを変更しよう
+// P75 List3-2 PlayerController
+// Unityエディターからパラメーターを変更しよう
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rbody;  // RigidBody2D型の変数
-    float axisH = 0.0f; // 入力
+    Rigidbody2D rbody;          // Rigidbody2D型の変数
+    float axisH = 0.0f;         //入力
     public float speed = 3.0f;  // 移動速度
-
-    // Start is called before the first frame update
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // Rigidbody2Dを取ってくる
         rbody = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // 水平方向の入力をチェックする
+        //水平方向の入力をチェックする
         axisH = Input.GetAxisRaw("Horizontal");
         // 向きの調整
-        if( axisH > 0.0f )
+        if (axisH > 0.0f)
         {
             // 右移動
             Debug.Log("右移動");
             transform.localScale = new Vector2(1, 1);
         }
-        else if(axisH < 0.0f )
+        else if (axisH < 0.0f)
         {
             // 左移動
             Debug.Log("左移動");
-            transform.localScale = new Vector2(-1, 1);  // 左右反転させる
+            transform.localScale = new Vector2(-1, 1); // 左右反転させる
         }
     }
-
-    private void FixedUpdate()
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        // 速度を更新する
-        rbody.linearVelocity = new Vector2(speed*axisH, rbody.linearVelocity.y);
+        //速度を更新する
+        rbody.linearVelocity = new Vector2(axisH * speed, rbody.linearVelocity.y);
     }
 }
