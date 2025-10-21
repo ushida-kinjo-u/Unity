fc97b5c2d0b739bf32ee966c4d92139acd3e0f5f
ushida-kinjo-u <109647066+ushida-kinjo-u@users.noreply.github.com>
2025-10-21 14:12:16 +0900

    Update List4_2.cs

--- SideViewGame/PlayerController/List4_2.cs
+++ SideViewGame/PlayerController/List4_2.cs
@@ -1,23 +1,20 @@
-// List 4-2 アニメーションさせるためにスクリプトを変更しよう
+// P121 List4-2 PlayerController.cs
+// アニメーションを切り替えるためにスクリプトを変更しよう
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rbody;  // RigidBody2D型の変数
-    float axisH = 0.0f; // 入力
-    public float speed = 3.0f;  // 移動速度
-    public float jump = 9.0f;   // ジャンプ力
+    Rigidbody2D rbody;              // Rigidbody2D型の変数
+    float axisH = 0.0f;             // 入力
+    public float speed = 3.0f;      // 移動速度
+    public float jump = 9.0f;       // ジャンプ力
     public LayerMask groundLayer;   // 着地できるレイヤー
-    bool goJump = false;        // ジャンプ開始フラグ
-    bool onGround = false;      // 地面に立っているフラグ
+    bool goJump = false;            // ジャンプ開始フラグ
+    bool onGround = false;          // 地面フラグ
 
     // アニメーション対応
-
     Animator animator; // アニメーター
-
     public string stopAnime = "PlayerStop";
     public string moveAnime = "PlayerMove";
     public string jumpAnime = "PlayerJump";
@@ -25,120 +22,95 @@ public class PlayerController : MonoBehaviour
     public string deadAnime = "PlayerOver";
     string nowAnime = "";
     string oldAnime = "";
-    
-    // Start is called before the first frame update
+
     void Start()
     {
-        // Rigidbody2Dを取ってくる
-        rbody = this.GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-	      nowAnime = stopAnime;
-        oldAnime = stopAnime;
+        rbody = this.GetComponent<Rigidbody2D>();   // Rigidbody2Dを取ってくる
+        animator = GetComponent<Animator>();        // Animator を取ってくる
+        nowAnime = stopAnime;                       // 停止から開始する
+        oldAnime = stopAnime;                       // 停止から開始する
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // 水平方向の入力をチェックする
-        axisH = Input.GetAxisRaw("Horizontal");
-
-        // 向きの調整
-        if (axisH > 0.0f)
+        // 地上判定
+        onGround = Physics2D.CircleCast(transform.position,    // 発射位置
+                                        0.2f,                  // 円の半径
+                                        Vector2.down,          // 発射方向
+                                        0.0f,                  // 発射距離
+                                        groundLayer);          // 検出するレイヤー
+                                                               // キャラクターをジャンプさせる
+        if (Input.GetButtonDown("Jump"))
         {
-            // 右移動
-            Debug.Log("右移動");
-            transform.localScale = new Vector2(1, 1);
+            goJump = true; // ジャンプフラグを立てる
+        }
+
+        axisH = Input.GetAxisRaw("Horizontal");     //水平方向の入力をチェックする
+        if (axisH > 0.0f)                           // 向きの調整
+        {
+            transform.localScale = new Vector2(1, 1);   // 右移動
         }
         else if (axisH < 0.0f)
         {
-            // 左移動
-            Debug.Log("左移動");
-            transform.localScale = new Vector2(-1, 1);  // 左右反転させる
+            transform.localScale = new Vector2(-1, 1); // 左右反転させる
         }
 
-        // キャラクターをジャンプさせる
-        if (Input.GetButtonDown("Jump"))
+        // アニメーション更新
+        if (onGround)       // 地面の上
         {
-            Jump(); // ジャンプ
-        }
-    }
-
-    void FixedUpdate()
-    {
-        // 地上判定
-        onGround = Physics2D.Linecast(transform.position,
-                                      transform.position - (transform.up * 1.0f),
-                                      groundLayer);
-
-        if (onGround || axisH != 0)
-        {
-            // 地面の上 or 速度が 0 ではない
-            // 速度を更新する
-            rbody.linearVelocity = new Vector2(speed*axisH, rbody.linearVelocity.y);
-        }
-
-        if (onGround && goJump)
-        {
-            // 地面の上でジャンプキーが押された
-            // ジャンプさせる
-            Debug.Log(" ジャンプ! ");
-
-            Vector2 jumpPw = new Vector2(0, jump);        // ジャンプさせるベクトルを作る
-            rbody.AddForce(jumpPw, ForceMode2D.Impulse);  // 瞬間的な力を加える
-            goJump = false; // ジャンプフラグを下ろす
-        }
-
-        if (onGround)
-        {
-            // 地面の上
             if (axisH == 0)
             {
-               nowAnime = stopAnime; // 停止中
+                nowAnime = stopAnime; // 停止中
             }
             else
             {
                 nowAnime = moveAnime; // 移動
             }
         }
-        else
+        else                // 空中
         {
-            // 空中
             nowAnime = jumpAnime;
         }
-
         if (nowAnime != oldAnime)
         {
             oldAnime = nowAnime;
             animator.Play(nowAnime); // アニメーション再生
         }
     }
-
-    // ジャンプ
-    public void Jump()
+    
+    void FixedUpdate()
     {
-        goJump = true;  // ジャンプフラグを立てる
-        Debug.Log("ジャンプボタン押し！");
+        if (onGround || axisH != 0)     // 地面の上 or 速度が 0 ではない
+        {
+            //速度を更新する
+            rbody.linearVelocity = new Vector2(axisH * speed, rbody.linearVelocity.y);
+        }
+        if (onGround && goJump)         // 地面の上でジャンプキーが押された
+        {
+            // ジャンプさせる
+            Vector2 jumpPw = new Vector2(0, jump);          // ジャンプさせるベクトルを作る
+            rbody.AddForce(jumpPw, ForceMode2D.Impulse);    // 瞬間的な力を加える
+            goJump = false;                                 // ジャンプフラグを下ろす
+        }
     }
 
     // 接触開始
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Goal"))
+        if (collision.gameObject.tag == "Goal")
         {
-            Goal();        // ゴール！！
+            Goal();         // ゴール！！
         }
-        else if (collision.gameObject.CompareTag("Dead"))
+        else if (collision.gameObject.tag == "Dead")
         {
             GameOver();     // ゲームオーバー
         }
     }
-
     // ゴール
     public void Goal()
     {
         animator.Play(goalAnime);
     }
-
     // ゲームオーバー
     public void GameOver()
     {
